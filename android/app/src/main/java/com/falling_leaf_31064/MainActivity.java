a1aa2fe1be3d686db7af3fb178da179e46d7b1db
Crowdbotics <team@crowdbotics.com>
2021-10-03 06:20:03 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/falling_leaf_31064/MainActivity.java
@@ -0,0 +1,15 @@
+package com.falling_leaf_31064;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "falling_leaf_31064";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "falling_leaf_31064",
+  url: "https://falling_leaf_31064.botics.co",
+  api: "https://falling_leaf_31064.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
